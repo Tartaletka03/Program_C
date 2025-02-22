bfee966aed8765a89bf90b58f657e893f4ac1195
Nikita <nikita.malyshev0007@gmail.com>
2025-02-22 14:07:01 +1000

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <math.h>
 
-/*ЗАДАНИЕ 4
+double triangle_perimeter(double a, double b, double c) {
+    return a + b + c;
+}
 
-Написать функции, вычисляющие периметр и площадь треугольника по значениям длин трёх его сторон. 
-Разместить их в отдельном модуле (отличном от того, где располагается функция main( )). 
-Ввести с консоли значения длин сторон треугольника (с проверкой на корректность неравенства треугольника) 
-и выдать на консоль вычисленные с помощью написанных функций значения его периметра и площади.*/
+double triangle_area(double a, double b, double c) {
+    double s = (a + b + c) / 2.0;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
 
 
 int main(int argc, char *argv[]){
+    double a, b, c;
+
+    printf("Стороны треугольника a b c: ");
+    if (scanf("%lf %lf %lf", &a, &b, &c) != 3) {
+        fprintf(stderr, "Числа нужно было 3\n");
+        return 1;
+    }
+
+    // Проверка на корректность неравенства треугольника
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+        fprintf(stderr, "Не бывает такого\n");
+        return 1;
+    }
+
+    // Вычисляем периметр и площадь, используя функции из triangle_utils.c
+    double perimeter = triangle_perimeter(a, b, c);
+    double area = triangle_area(a, b, c);
+
+    printf("Периметр треугольника: %.2lf\n", perimeter);
+    printf("Площадь треугольника: %.2lf\n", area);
 
     return 0;
 }
